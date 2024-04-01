44779ebc297d18d74bdd03660df8f3d8b4f251d0
bhagat-abhishek <inst.abhishek@gmail.com>
2024-04-01 20:22:16 +0530

    using fetch()

---
+++ promises/one-demo.js
@@ -0,0 +1,21 @@
+// async function getAllUsers() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = await response.json();
+
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// getAllUsers();
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => console.log(error));
